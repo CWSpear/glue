24a6742e609409ba09de68232161f97eb60291b1
Cameron Spear <cameronspear@gmail.com>
2014-05-09 10:35:45 -0700

    Treat an API key of "APIKEYGOESHERE" the same as an empty API key.

--- api/policies/apiKey.js
+++ api/policies/apiKey.js
@@ -7,11 +7,11 @@ module.exports = function hasApiKey (req, res, next) {
     if ((req.user || {}).id && apiKey === defaultUser.apiKey) return next();
 
     // if we have neither, let's just stop them now
-    if (!apiKey)
-        return res.forbidden('Please register for an API Key.');
+    if (!apiKey || apiKey === 'APIKEYGOESHERE')
+        return res.forbidden('Please register for an API key.');
 
     User.findOne({ apiKey: apiKey }, function (err, user) {
-        if (err || !(user || {}).id) return res.forbidden('Invalid/expired API Key.');
+        if (err || !(user || {}).id) return res.forbidden('Invalid/expired API key.');
         req.user = user;
         next();
     });
