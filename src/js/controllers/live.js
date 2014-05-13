b1330f9fdbac1a8d9325033315560c0a4db8b457
Cameron Spear <cameronspear@gmail.com>
2014-05-13 01:07:27 -0700

    Well... at least it's updating and we have sails.io installed and "working."

--- src/js/controllers/live.js
+++ src/js/controllers/live.js
@@ -3,9 +3,8 @@ angular.module('glue')
 
 .controller('LiveCtrl', ($scope, $rootScope, $routeParams, $location, Restangular) => {
     $rootScope.ensureUser.then(() => {
-        return $rootScope.user.one('snippets', $routeParams.id).get();
-    }).then(snippets => {
-        var snippet = snippets[0];
+        return Restangular.one('snippets', $routeParams.id).get();
+    }).then(snippet => {
         $scope.snippet = snippet;
         $rootScope.aceConfig.mode = $scope.snippet.language;
         $rootScope.aceConfig.tabSize = $scope.snippet.tabSize || 4;
@@ -13,9 +12,18 @@ angular.module('glue')
     }).catch(function (err) {
         if (err.status == 404) {
             // TODO: better 404 handling
+            $scope.snippet = { snippet: 'Snippet not found.' };
+        } else if (err.status == 403) {
             $scope.snippet = { snippet: 'You don\'t have access to edit this snippet.' };
         }
 
         // TODO: catchall error handling
     });
+
+    var updateSnippet = function (snippet, old) {
+        if (!snippet || snippet === old || !$scope.snippet.put) return;
+        $scope.snippet.put();
+    };
+
+    $scope.$watch('snippet.snippet', updateSnippet);
 });
