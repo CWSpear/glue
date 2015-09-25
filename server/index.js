68c36f0bb7d5b49767bc730db7ab66a595ce638f
Justen Martin <justen@justenmartin.com>
2015-09-25 18:45:15 -0500

    fixed lint errors

--- server/index.js
+++ server/index.js
@@ -4,24 +4,19 @@ var boot = require('loopback-boot');
 var app = module.exports = loopback();
 
 app.start = () => {
-  // start the web server
-  return app.listen(() => {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+    // start the web server
+    return app.listen(() => {
+        app.emit('started');
+    });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, (err) => {
-  if (err) throw err;
+    if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+    // start the server if `$ node server.js`
+    if (require.main === module) {
+        app.start();
+    }
 });
